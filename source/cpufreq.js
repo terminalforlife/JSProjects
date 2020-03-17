45a8238f106f1ebbfbd815ca1b9ba578454f2ca4
terminalforlife <terminalforlife@yahoo.com>
2020-03-17 21:59:31 +0000

    Add `Err()` function (woo!) to `cpufreq.js`
    Also nailed the `Progrm` variable, in making it dynamic, but it's a bit
    convoluted; is there a better way?

--- source/cpufreq.js
+++ source/cpufreq.js
@@ -3,7 +3,7 @@
 //-----------------------------------------------------------------------------
 // Project Name      - JSProjects/source/cpufreq.js
 // Started On        - Wed 11 Mar 16:13:33 GMT 2020
-// Last Change       - Tue 17 Mar 21:43:07 GMT 2020
+// Last Change       - Tue 17 Mar 21:59:26 GMT 2020
 // Author E-Mail     - terminalforlife@yahoo.com
 // Author GitHub     - https://github.com/terminalforlife
 //-----------------------------------------------------------------------------
@@ -11,15 +11,18 @@
 //-----------------------------------------------------------------------------
 
 const os = require('os')
+const path = require('path')
 
 var CurVer = '2020-03-11'
-var Progrm = 'lsbins'
+var Progrm = path.basename(process.argv[1]).split('.')[0]
+
+function Err(){
+	console.error('ERROR: ' + arguments[1])
+	arguments[0] > 0 && process.exit(arguments[0])
+}
 
 var CoreNum = 0
 while (CoreNum <= os.cpus().length - 1){
-	console.log(
-		'Core_' + CoreNum + ': ' + os.cpus()[CoreNum].speed + 'Hz'
-	)
-
+	console.log('Core_' + CoreNum + ': ' + os.cpus()[CoreNum].speed + 'Hz')
 	CoreNum++
 }
