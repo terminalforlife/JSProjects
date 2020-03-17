1fa8d30f8227a00ed2dc6346c810b679723b7f81
terminalforlife <terminalforlife@yahoo.com>
2020-03-17 21:43:23 +0000

    Have `cpufreq.js` make use of however many cores

--- source/cpufreq.js
+++ source/cpufreq.js
@@ -3,7 +3,7 @@
 //-----------------------------------------------------------------------------
 // Project Name      - JSProjects/source/cpufreq.js
 // Started On        - Wed 11 Mar 16:13:33 GMT 2020
-// Last Change       - Wed 11 Mar 19:52:07 GMT 2020
+// Last Change       - Tue 17 Mar 21:43:07 GMT 2020
 // Author E-Mail     - terminalforlife@yahoo.com
 // Author GitHub     - https://github.com/terminalforlife
 //-----------------------------------------------------------------------------
@@ -15,9 +15,11 @@ const os = require('os')
 var CurVer = '2020-03-11'
 var Progrm = 'lsbins'
 
-console.log(
-	'Core_0: ' + os.cpus()[0].speed + 'Hz\n' +
-	'Core_1: ' + os.cpus()[1].speed + 'Hz\n' +
-	'Core_2: ' + os.cpus()[2].speed + 'Hz\n' +
-	'Core_3: ' + os.cpus()[3].speed + 'Hz'
-)
+var CoreNum = 0
+while (CoreNum <= os.cpus().length - 1){
+	console.log(
+		'Core_' + CoreNum + ': ' + os.cpus()[CoreNum].speed + 'Hz'
+	)
+
+	CoreNum++
+}
