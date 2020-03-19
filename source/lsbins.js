676309a166bdcca6675da417b77099207fb03656
terminalforlife <terminalforlife@yahoo.com>
2020-03-19 21:04:33 +0000

    Update `lsbins.js` with `Usage()` & `Err()`
    Just not sure why the array won't stay sorted. Pretty poor sorting
    implementation compared to what I'm used to, like in Perl.

--- source/lsbins.js
+++ source/lsbins.js
@@ -3,7 +3,7 @@
 //-----------------------------------------------------------------------------
 // Project Name      - JSProjects/source/lsbins.js
 // Started On        - Wed 11 Mar 16:13:33 GMT 2020
-// Last Change       - Thu 19 Mar 03:21:11 GMT 2020
+// Last Change       - Thu 19 Mar 21:04:11 GMT 2020
 // Author E-Mail     - terminalforlife@yahoo.com
 // Author GitHub     - https://github.com/terminalforlife
 //-----------------------------------------------------------------------------
@@ -14,6 +14,19 @@ const fs = require('fs')
 const os = require('os')
 const path = require('path')
 
+function Err(Int, Str){
+	console.error('ERROR: ' + Str)
+	if (Int > 0){process.exit(Int)}
+}
+
+function Usage(){
+	console.log(
+		'Usage: ' + Progrm + ' [OPTS]\n\n' +
+		'  -h, --help               - Display this help information.\n' +
+		'  -v, --version            - Output the version datestamp.'
+	)
+}
+
 var CurVer = '2020-03-19'
 var Progrm = path.basename(__filename.split('\.')[0])
 
@@ -21,10 +34,12 @@ process.argv.shift()
 process.argv.shift()
 
 while (process.argv.length > 0){
-	if (process.argv[0].match('^(--version|-v)$')){
+	if (process.argv[0].match('^(--help|-h)$')){
+		Usage(); process.exit(0)
+	}else if (process.argv[0].match('^(--version|-v)$')){
 		console.log(CurVer)
 	}else{
-		console.error('ERROR: Unrecognised option(s) provided.')
+		Err(1, 'Unrecognised option(s) provided.')
 	}
 
 	process.argv.shift()
